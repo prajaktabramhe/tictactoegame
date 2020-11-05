7737fb2e8695952e4971e7dc17451c6a31d067f8
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-11-05 10:49:17 +0530

    welcome

---
+++ TicTacToeGame.java
@@ -0,0 +1,16 @@
+package tictoctoegame;
+
+public class TicTacToeGame 
+{
+	
+	public static void main(String[] args) 
+	{
+		char[] board = new char [10];
+		for(int index=0;index<10;index++)
+		{
+			board[index]=' ';
+		}
+		
+    }
+
+}
