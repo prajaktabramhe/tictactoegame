942a03f45aafd421e29a5d8b711d937e064772aa
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-11-05 11:48:26 +0530

    take the input from user & choose a letter X or O

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,15 +1,36 @@
 package tictoctoegame;
-
+import java.util.*;
 public class TicTacToeGame 
 {
-	
+    public char player,computer;
+	public void determinesymbol()
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your Symbol:");
+		char c = sc.next().charAt(0);
+		 if (c=='X')
+		 {
+			 player='X';
+			 computer='O';
+			 System.out.println("player:"+player+" computer:"+computer);
+		 }
+		 else
+		 {
+			 player='O';
+			 computer='X';
+			 System.out.println("player:"+player+" computer:"+computer);
+		 }
+		 
+	}
 	public static void main(String[] args) 
 	{
 		char[] board = new char [10];
 		for(int index=0;index<10;index++)
-		{
+	    {
 			board[index]=' ';
 		}
+		TicTacToeGame obj1 = new TicTacToeGame();
+		obj1.determinesymbol();
 		
     }
 
