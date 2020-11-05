3d080e938ed6a8743f30656b776794e2cbb9ab13
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-11-05 10:32:09 +0530

    create tictoctoe game

---
+++ tictoctoe.java
@@ -0,0 +1,21 @@
+package tictoctoegame;
+
+public class tictoctoe
+{
+    public void iswin()
+    {
+    	
+    }
+	public static void main(String[] args) {
+		char[] arr1 = new char [10];
+		for(char c: arr1)
+		{
+			System.out.println(c);
+		}
+         tictoctoe obj1 = new tictoctoe();
+         obj1.iswin();
+	}
+	
+
+
+}
